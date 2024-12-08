6588cf31bffad5ebade16fff6a0837626be5290b
OptimaAmar <optimaamar6@gmail.com>
2024-12-08 07:51:50 +0700

    first commit

---
+++ app/Views/data_diri.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data diri Mahasiswa</title>
+    <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
+</head>
+<body>
+<div class="container mt-3">
+    <h1>Data Diri Optima Amar</h1>
+    <hr>    
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+            <div class="col-md-4">
+            <img src="<?= base_url('foto/foto.jpg') ?>" alt="Optima Amar"class="img-fluid rounded-start">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h6>Nama            : Optima Amar</h6>
+                <h6>Nim             : 23.240.0069</h6>
+                <h6>kelas           : 3P41A</h6>
+                <h6>Jenis Kelamin   : Laki-laki</h6>
+                <h6>Alamat          : Kota Pekalongan </h6>
+                <h6>No hp           : 085601603521 </h6>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                <a href="<?= base_url('') ?>"></a>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html> 
